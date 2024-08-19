a3ad11e1cf949e6a90a546d8027a48ecdb03df32
Swastika Gupta <swastgupta@microsoft.com>
2024-08-19 20:20:45 +0530

    Hands on Angular Assignment 1

--- src/app/components/empty/empty.component.ts
+++ src/app/components/empty/empty.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { Participant } from '../../models/participant.model';
+import { ParticipantService } from '../../services/participant.service';   
+
 
 @Component({
   selector: 'app-empty',
   templateUrl: './empty.component.html',
-  styleUrl: './empty.component.scss'
+  styleUrls: ['./empty.component.scss']
 })
-export class EmptyComponent {
+export class EmptyComponent   
+ {
+  participants: Participant[] = [];
 
+  constructor(private participantService: ParticipantService) {}
+
+  ngOnInit() {
+    this.participantService.getParticipants().subscribe(participants => {
+      this.participants = participants;
+    });
+  }
+
+  get hasParticipants(): boolean {
+    return this.participants.length > 0;
+  }
 }
--- src/app/services/participant.service.ts
+++ src/app/services/participant.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { Participant } from '../models/participant.model';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ParticipantService {
 
-  constructor() { }
+  participants: Participant[] = [];
+  apiUrl = 'http://localhost:3000/participants';
+
+  constructor(private http: HttpClient) { }
+
+  getParticipants(){
+    return this.http.get<Participant[]>(this.apiUrl);
+  }
+
+  addParticipants(participant: Participant) {
+    return this.http.post<Participant>(this.apiUrl, participant);
+  }
 }
